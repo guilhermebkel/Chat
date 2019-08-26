7ebd84094ffca523fbbbf8d5902a9e082ae1f2a7
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-08-25 22:32:38 -0300

    Added message handler

--- index.ts
+++ index.ts
@@ -3,16 +3,19 @@ import { createServer } from 'http'
 import * as socket from 'socket.io'
 import * as path from 'path'
 import * as ejs from 'ejs'
+import { throws } from 'assert';
 
 class Server {
     _app: any = express()
     _server: any = createServer(this._app)
     _io: any = socket(this._server)
+    _messages: any = []
 
     constructor(){
         this.serveStaticFiles()
         this.renderStaticFiles()
         this.startServer()
+        this.handleConnection()
     }
 
     serveStaticFiles(){
@@ -31,6 +34,16 @@ class Server {
     startServer(){
         this._server.listen(process.env.PORT || 3000, () => {
             console.log(`=> Server running... [PORT ${process.env.PORT || 3000}]`)
+        })
+    }
+
+    handleConnection(){
+        this._io.on('connection', socket => {
+            console.log(`Socket connected: ${socket.id}`)
+            
+            socket.on('sendMessage', data => {
+                this._messages.push(data)
+            })
         })
     }
 }
